dddc64abb8b2cbb3bedf21610807f6053c247300
greenkoopa <green.koopa667@gmail.com>
2018-07-18 01:19:38 -0400

    New homepage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import Homepage from './components/Homepage/Homepage'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Homepage />, document.getElementById('root')
+);
